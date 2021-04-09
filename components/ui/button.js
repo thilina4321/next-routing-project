5794bf8825bc21de52344279e43fb9dd53b1af4f
DESKTOP-FRK722U\Thilina <dilshanthilina53@gmail.com>
2021-04-09 21:28:54 +0530

    some fixes

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,19 +1,23 @@
-import {useRouter} from 'next/router'
-import classes from './button.module.css'
+import { useRouter } from "next/router";
+import ArrowIcon from "./arrow-icon";
+import classes from "./button.module.css";
 
 const Button = (props) => {
-    const router = useRouter()
-    const {page} = props
+  const router = useRouter();
+  const { page } = props;
 
-    const nextPage = ()=>{
-        router.push(page)
-    }
+  const nextPage = () => {
+    router.push(page);
+  };
 
-    return (
-        <div>
-            <button className={classes.btn} onClick={nextPage} type="button"> Explore More  </button>
-        </div>
-    )
-}
+  return (
+    
+      <button className={classes.btn} onClick={nextPage} type="button">
+        <span> Explore More </span>
+        <span className={classes.arrow}> <ArrowIcon /> </span>
+      </button>
+    
+  );
+};
 
-export default Button
+export default Button;
